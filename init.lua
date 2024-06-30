076e5d149a44836b1a33163698500b5b0c768748
Lucca Jiménez Könings <development@jimkoen.com>
2024-06-30 03:53:17 +0200

    add typescript tools

--- init.lua
+++ init.lua
@@ -875,6 +875,11 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+    'pmizio/typescript-tools.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    opts = {},
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
